8778a12d8b5e65e3158afff8a70b7d7f5089758c
giorgi gio <gioshvelidze39@gmail.com>
2025-08-07 17:18:19 +0400

    araferia mara gaibaga

--- SFMLtest/characters.cpp
+++ SFMLtest/characters.cpp
@@ -136,7 +136,7 @@ NPC::NPC(const string& texturePath, int startX, int startY) : Entity(texturePath
 // This is the talk function for NPCs
 void NPC::talk(RenderWindow& window, Human& player) {
     float bottomEdge = window.getSize().y;
-    DialogueBox dialogBox(990.f, 200.f, 1.f, bottomEdge - 150.f, Color(0, 0, 0, 128), Color::Red);
+    DialogueBox dialogBox(998.f, 200.f, 1.f, bottomEdge - 150.f, Color(0, 0, 0, 128), Color::Red);
     dialogBox.setText( "Hello Adventurer \n ");
     dialogBox.addtext("Press an Enter to continue ...");
 
